2d5e63728cdb87bd4180ebcf9c60b84729de7c45
cjordanball <cjordanball@gmail.com>
2025-06-03 01:27:52 +0000

    update page.tsx file

---
+++ hosting/app/page.tsx
@@ -0,0 +1,14 @@
+'use clien';
+import { DataContext, DataContextProvider } from '../src/store/data-context';
+import { MainGrid } from '../src/components/mainGrid';
+import styles from './page.module.css';
+
+export default function Page() {
+  return (
+    <DataContextProvider>
+      <div>
+        <MainGrid />
+      </div>
+    </DataContextProvider>
+  )
+}
