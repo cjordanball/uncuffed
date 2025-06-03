a30a5e35ad76acb71aaa12678529ec4d054ce274
Jordan Ball <cjordanball@ballwebdev.com>
2025-06-02 21:40:14 -0400

    add module styles to page

--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -4,11 +4,12 @@ import { MainGrid } from '../src/components/mainGrid';
 import styles from './page.module.css';
 
 export default function Page() {
-  return (
-    <DataContextProvider>
-      <div>
-        <MainGrid />
-      </div>
-    </DataContextProvider>
-  )
+	return (
+		<DataContextProvider>
+			<div className={styles.test}>
+				<MainGrid />
+				<h1>TEST</h1>
+			</div>
+		</DataContextProvider>
+	);
 }
