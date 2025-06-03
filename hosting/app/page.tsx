25196a4f38844eb8a2db545a1ade749302eafff0
Jordan Ball <cjordanball@ballwebdev.com>
2025-06-02 22:40:21 -0400

    fix dependencies

--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { DataContext, DataContextProvider } from '../../src/store/data-context';
+import { DataContextProvider } from '../../src/store/data-context';
 import { MainGrid } from '../../src/components/mainGrid';
 import styles from './page.module.css';
 
