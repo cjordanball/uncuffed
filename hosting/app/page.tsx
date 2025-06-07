65a11b1fb22348b4cfd381519a4fe969af7fe384
cjordanball <cjordanball@gmail.com>
2025-06-07 18:23:10 +0000

    update main page

--- hosting/app/page.tsx
+++ hosting/app/page.tsx
@@ -1,14 +1,22 @@
 'use client';
 import { DataContextProvider } from '../../src/store/data-context';
 import { MainGrid } from '../../src/components/mainGrid';
+import { Instructions } from '../../src/components/instructions';
 import styles from './page.module.css';
 
 export default function Page() {
 	return (
 		<DataContextProvider>
-			<div className={styles.container}>
-				<MainGrid />
+			<div className={styles.overlay}>
+				<div className={styles.heading}>
+					<h1 className={styles.title}>Uncuffed</h1>
+					<span className={styles.subtitle}>Red Flag Bingo</span>
+				</div>
+				<Instructions />
+				<div className={styles.container}>
+					<MainGrid />
+				</div>
 			</div>
 		</DataContextProvider>
 	);
-}
+};
