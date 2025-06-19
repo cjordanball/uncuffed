8afea6e20320361d717ac3bc3f8f8fb456bd2979
Jordan Ball <cjordanball@ballwebdev.com>
2025-06-18 20:55:12 -0400

    add game board back

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 'use client';
 import { DataContextProvider } from '../store/data-context';
-// import { MainGrid } from '../components/mainGrid';
+import { MainGrid } from '../components/mainGrid';
 import Image from 'next/image';
 import uncuffed from '../../public/images/uncuffed.jpg';
-// import { Instructions } from '../components/instructions';
+import { Instructions } from '../components/instructions';
 import styles from './page.module.css';
 
 export default function Page() {
@@ -11,13 +11,13 @@ export default function Page() {
 		<DataContextProvider>
 			<div className={styles.container}>
 				<Image alt='background' className={styles.image} src={uncuffed} />
-				{/* <div className={styles.sheet}></div>
+				<div className={styles.sheet}></div>
 				<div className={styles.overlay}>
 					<Instructions />
 					<div className={styles.containor}>
 						<MainGrid />
 					</div>
-				</div> */}
+				</div>
 			</div>
 		</DataContextProvider>
 	);
